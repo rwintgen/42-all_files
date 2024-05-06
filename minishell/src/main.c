2eab5a0e8e34f1e523876049770a915af279f719
Romain Wintgens <rwintgen@1A5.42angouleme.fr>
2024-05-06 14:57:39 +0200

    TODO update

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 16:59:56 by amalangi          #+#    #+#             */
-/*   Updated: 2024/05/06 13:19:16 by rwintgen         ###   ########.fr       */
+/*   Updated: 2024/05/06 14:54:37 by rwintgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int g_sig;
 
 ///////// TODO ////////// 
 // make builtins
-//// export : segfault free_sh after exporting var then ctrl+D
 //// fix exit
+// remove whitespaces around commands
 // fix ctrl+C heredoc
 // valgrind (open FDs, leaks)
 // fix error msg char by char
