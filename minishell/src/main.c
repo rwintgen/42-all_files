7d9d64e5e4d50e1153736a46bbb885456e256c03
Romain Wintgens <rwintgen@1A1.42angouleme.fr>
2024-05-07 12:48:31 +0200

    backup all tests ok - 408/423

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 16:59:56 by amalangi          #+#    #+#             */
-/*   Updated: 2024/05/06 14:54:37 by rwintgen         ###   ########.fr       */
+/*   Updated: 2024/05/07 12:47:48 by rwintgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,6 @@
 int g_sig;
 
 ///////// TODO ////////// 
-// make builtins
-//// fix exit
-// remove whitespaces around commands
 // fix ctrl+C heredoc
 // valgrind (open FDs, leaks)
 // fix error msg char by char
