bdd2788817f874e7a4d2a207a8bb2bf7d9b69f65
Romain Wintgens <rwintgen@1A3.42angouleme.fr>
2024-05-27 16:49:39 +0200

    TODO update

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 16:59:56 by amalangi          #+#    #+#             */
-/*   Updated: 2024/05/27 16:18:05 by rwintgen         ###   ########.fr       */
+/*   Updated: 2024/05/27 16:49:28 by rwintgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	g_sig;
 
 ///////// TODO //////////
-// Non-builtins do not exec after export
+// All good so far!
 /////////////////////////
 
 /*
