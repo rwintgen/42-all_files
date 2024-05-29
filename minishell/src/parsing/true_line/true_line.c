524590cb2425a1490ad755f4aab3f1a39a75a6ad
Romain Wintgens <rwintgen@1A1.42angouleme.fr>
2024-05-29 14:18:34 +0200

    tests OK, fixing echo $

--- minishell/src/parsing/true_line/true_line.c
+++ minishell/src/parsing/true_line/true_line.c
@@ -6,7 +6,7 @@
 /*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 12:40:53 by deymons           #+#    #+#             */
-/*   Updated: 2024/05/28 14:59:37 by rwintgen         ###   ########.fr       */
+/*   Updated: 2024/05/29 14:16:14 by rwintgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,13 @@ char	*true_line(char *str, t_sh *sh)
 		return (NULL);
 	}
 	add_spaces(&formatted, str);
-	// printf("formatted: \"%s\"\n", formatted); // DEBUG
+	printf("formatted: \"%s\"\n", formatted); // DEBUG
 	free(str);
 	tmp = formatted;
 	formatted = var_expand(formatted, sh->envp, sh->exit_code);
 	if (tmp != formatted)
 		free(tmp);
-	// printf("expanded: \"%s\"\n", formatted); // DEBUG
+	printf("expanded: \"%s\"\n", formatted); // DEBUG
 	return (formatted);
 }
 
