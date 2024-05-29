f73c44afc1d5d393b27d32b3d463211a15489a72
Romain Wintgens <rwintgen@1A1.42angouleme.fr>
2024-05-29 14:43:42 +0200

    fixed cd err_msg

--- minishell/src/builtins/ft_cd_utils.c
+++ minishell/src/builtins/ft_cd_utils.c
@@ -6,7 +6,7 @@
 /*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 17:16:03 by rwintgen          #+#    #+#             */
-/*   Updated: 2024/05/24 14:23:36 by rwintgen         ###   ########.fr       */
+/*   Updated: 2024/05/29 14:41:14 by rwintgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,13 +73,7 @@ void	update_cwd(t_envp *envp, char *new_cwd)
 
 int	err_msg_cd(char *old_cwd, char *new_cwd)
 {
-	char	*result;
-
-	result = ft_strjoin("minishell: cd: ", new_cwd);
-	result = ft_strjoin(result, ": ");
-	result = ft_strjoin(result, strerror(errno));
-	write(STDERR_FILENO, result, ft_strlen(result));
-	ft_putstr_fd("\n", STDERR_FILENO);
+	print_err("minishell: cd: ", new_cwd, ": ", strerror(errno));
 	free(old_cwd);
 	return (FAILURE);
 }
