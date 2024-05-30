e0d18d26da20e63565462efdf53c885d2b1228f0
Romain Wintgens <rwintgen@3C6.42angouleme.fr>
2024-05-30 17:02:07 +0200

    init envp if env -i

--- minishell/src/exec/envp/envp.c
+++ minishell/src/exec/envp/envp.c
@@ -6,11 +6,13 @@
 /*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:44:40 by deymons           #+#    #+#             */
-/*   Updated: 2024/05/30 16:01:47 by rwintgen         ###   ########.fr       */
+/*   Updated: 2024/05/30 16:58:03 by rwintgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	create_envp(t_envp **envp);
 
 // converts a char **envp to a t_envp linked list
 t_envp	*save_envp(char **env, t_sh *tofree)
@@ -27,6 +29,8 @@ t_envp	*save_envp(char **env, t_sh *tofree)
 		set_value(&env_cpy, env[i]);
 		i++;
 	}
+	if (i == 0)
+		create_envp(&env_cpy);
 	return (env_cpy);
 }
 
@@ -57,4 +61,28 @@ char	**restore_envp(t_envp *envp)
 	}
 	result[i] = NULL;
 	return (result);
+}
+
+static void	create_envp(t_envp **envp)
+{
+	t_envp	*new_node;
+
+	new_node = malloc(sizeof(t_envp));
+	if (!new_node)
+		return ;
+	new_node->key = ft_strdup("PWD");
+	new_node->value = getcwd(NULL, 0);
+	new_node->envar = assemble_envar(new_node->key, new_node->value);
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	*envp = new_node;
+
+	t_envp *oldpwd_node = malloc(sizeof(t_envp));
+	if (!oldpwd_node)
+		return ;
+	oldpwd_node->key = ft_strdup("OLDPWD");
+	oldpwd_node->value = NULL;
+	oldpwd_node->prev = *envp;
+	oldpwd_node->next = NULL;
+	(*envp)->next = oldpwd_node;
 }
