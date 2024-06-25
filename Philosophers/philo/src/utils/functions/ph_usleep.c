be17cf4100ecba7934b57424c8d270910a774086
Romain Wintgens <rwintgen@2A4.42angouleme.fr>
2024-06-25 18:28:57 +0200

    fixed ph_usleep bug when very high time to eat

--- Philosophers/philo/src/utils/functions/ph_usleep.c
+++ Philosophers/philo/src/utils/functions/ph_usleep.c
@@ -6,7 +6,7 @@
 /*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 15:52:19 by rwintgen          #+#    #+#             */
-/*   Updated: 2024/06/19 16:50:15 by rwintgen         ###   ########.fr       */
+/*   Updated: 2024/06/25 18:26:47 by rwintgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,14 @@
 void	ph_usleep(long time, t_table *table)
 {
 	long	start;
-	long	remaining;
-	long	elapsed;
 
 	start = get_time(MICROSECONDS);
 	while (get_time(MICROSECONDS) - start < time)
 	{
 		if (dinner_finished(table))
 			break ;
-		elapsed = get_time(MICROSECONDS) - start;
-		remaining = time - elapsed;
-		if (remaining > 1e3)
-			usleep(remaining / 2);
-		else
-		{
-			while (get_time(MICROSECONDS) - start < time)
-				;
-		}
+		while (get_time(MICROSECONDS) - start < time \
+				&& !dinner_finished(table))
+			;
 	}
 }
