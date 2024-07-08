ad5751c86234c86c428feba68d1e90d305db9db5
Romain Wintgens <rwintgen@1E5.42angouleme.fr>
2024-07-08 12:30:19 +0200

    created cpp directory + cpp00 started

---
+++ cpp/cpp00/ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 11:52:58 by rwintgen          #+#    #+#             */
+/*   Updated: 2024/07/08 12:20:29 by rwintgen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::string result = "";
+
+	(void)argc;
+
+	if (!argv[1])
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		std::string tmp = argv[i];
+
+		for (int j = 0; j < (int)tmp.length(); j++)
+		{
+			result += std::toupper(tmp[j]);
+		}
+		result += ' ';
+	}
+	std::cout << result << std::endl;
+
+	return (0);
+}
