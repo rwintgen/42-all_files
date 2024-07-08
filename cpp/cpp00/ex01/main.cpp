c25d8ec03af1ffa5ad9164631662b7e7a5eda195
Romain Wintgens <rwintgen@1E5.42angouleme.fr>
2024-07-08 13:55:31 +0200

    pseudo-coded main.cpp

--- cpp/cpp00/ex01/main.cpp
+++ cpp/cpp00/ex01/main.cpp
@@ -6,7 +6,28 @@
 /*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 12:39:33 by rwintgen          #+#    #+#             */
-/*   Updated: 2024/07/08 12:39:37 by rwintgen         ###   ########.fr       */
+/*   Updated: 2024/07/08 13:53:40 by rwintgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <PhoneBook.hpp>
+#include <cstdio>
+
+int	main(void)
+{
+	std::string	input;
+	PhoneBook	phonebook;
+
+	while (true)
+	{
+		std::getline(std::cin, input);
+		if (input == "ADD")
+			//
+		else if (input == "SEARCH")
+			//
+		else if (input == "EXIT")
+			break ;
+	}
+	std::cout << "PhoneBook closed" << std::endl;
+	return (0);
+}
