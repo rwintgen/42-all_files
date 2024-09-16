d3a81b2dfde512531a80b3368694acef06f7170e
Romain Wintgens <rwintgen@1A5.42angouleme.fr>
2024-09-16 15:44:50 +0200

    added subject main and changed executable name

--- cpp/cpp04/ex03/main.cpp
+++ cpp/cpp04/ex03/main.cpp
@@ -6,13 +6,39 @@
 /*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 14:44:01 by rwintgen          #+#    #+#             */
-/*   Updated: 2024/09/16 14:48:04 by rwintgen         ###   ########.fr       */
+/*   Updated: 2024/09/16 15:43:29 by rwintgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
+#include "MateriaSource.hpp"
+#include "Character.hpp"
+#include "Ice.hpp"
+#include "Cure.hpp"
 
-int	main(void)
+int main(void)
 {
+	IMateriaSource* src = new MateriaSource();
+
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+
+	AMateria* tmp;
+
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+
+	ICharacter* bob = new Character("bob");
+	me->use(0, *bob);
+	me->use(1, *bob);
+
+	delete bob;
+	delete me;
+	delete src;
+
 	return (0);
 }
