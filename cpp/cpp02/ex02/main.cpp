7a38a66b731ef58b179db9247c700ccecc2449c7
Romain Wintgens <rwintgen@1B4.42angouleme.fr>
2024-09-23 10:46:36 +0200

    cpp02/ex02 more main tests

--- cpp/cpp02/ex02/main.cpp
+++ cpp/cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 12:41:15 by romain            #+#    #+#             */
-/*   Updated: 2024/09/12 12:14:15 by rwintgen         ###   ########.fr       */
+/*   Updated: 2024/09/23 10:44:48 by rwintgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,58 @@
 
 int	main(void)
 {
-	Fixed		a;
-	Fixed const	b(Fixed(5.05f) * Fixed(2));
+	// Fixed		a;
+	// Fixed const	b(Fixed(5.05f) * Fixed(2));
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max(a, b) << std::endl;
+	// std::cout << "Main tests:" << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << ++a << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << a++ << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << b << std::endl;
+	// std::cout << Fixed::max(a, b) << std::endl;
+
+	Fixed a(10);
+	Fixed b(5);
+	Fixed c(2.5f);
+
+	// Arithmetic Operators
+	std::cout << "Arithmetic Operators:" << std::endl;
+	std::cout << "a + b = " << (a + b) << std::endl;
+	std::cout << "a - b = " << (a - b) << std::endl;
+	std::cout << "a * b = " << (a * b) << std::endl;
+	std::cout << "a / b = " << (a / b) << std::endl;
+	std::cout << "a + c = " << (a + c) << std::endl;
+	std::cout << "a - c = " << (a - c) << std::endl;
+	std::cout << "a * c = " << (a * c) << std::endl;
+	std::cout << "a / c = " << (a / c) << std::endl;
+
+	// Comparison Operators
+	std::cout << "\nComparison Operators:" << std::endl;
+	std::cout << "a < b: " << (a < b) << std::endl;
+	std::cout << "a <= b: " << (a <= b) << std::endl;
+	std::cout << "a > b: " << (a > b) << std::endl;
+	std::cout << "a >= b: " << (a >= b) << std::endl;
+
+	// Increment/Decrement Operators
+	std::cout << "\nIncrement/Decrement Operators:" << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "++a: " << ++a << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "a++: " << a++ << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "--a: " << --a << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "a--: " << a-- << std::endl;
+	std::cout << "a: " << a << std::endl;
+
+	// Min/Max Functions
+	std::cout << "\nMin/Max Functions:" << std::endl;
+	std::cout << "min(a, b): " << Fixed::min(a, b) << std::endl;
+	std::cout << "max(a, b): " << Fixed::max(a, b) << std::endl;
+	std::cout << "min(a, c): " << Fixed::min(a, c) << std::endl;
+	std::cout << "max(a, c): " << Fixed::max(a, c) << std::endl;
+
 	return (0);
 }
