31d67a53768f7bb1476fb77537e40376eede58dc
Romain Wintgens <rwintgen@2D4.42angouleme.fr>
2024-11-19 11:34:23 +0100

    updated wordpress container

--- Inception/srcs/requirements/mariadb/mdb_init.sh
+++ Inception/srcs/requirements/mariadb/mdb_init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 service mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
---
+++ Inception/srcs/requirements/wordpress/wp_init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 5
+
+wp config create	--allow-root \
+					--dbname=$SQL_DATABASE \
+					--dbuser=$SQL_USER \
+					--dbpass=$SQL_PASSWORD \
+					--dbhost=mariadb:3306 --path='/var/www/wordpress'
