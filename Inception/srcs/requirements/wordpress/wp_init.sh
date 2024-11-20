20580b8ffaa4d92ae357525f13c5f813fa5803c9
Romain Wintgens <romain@mac-de-romain.home>
2024-11-20 17:21:28 +0100

    wordpress done & mdb update

--- Inception/srcs/requirements/wordpress/wp_init.sh
+++ Inception/srcs/requirements/wordpress/wp_init.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-sleep 5
+# let mdb_init.sh finish
+sleep 10
 
+# autofill wp startup fields
 wp config create	--allow-root \
 					--dbname=$SQL_DATABASE \
 					--dbuser=$SQL_USER \
 					--dbpass=$SQL_PASSWORD \
 					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+# create run/php dir if not exist
+if [ ! -d /run/php ]; then
+    mkdir ./run/php
+fi
+
+#launch php-fpm
+/usr/sbin/php-fpm7.3 -F
