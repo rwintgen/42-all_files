688c92e2f757991024bfff9f8fae6e44ecbd6175
Romain Wintgens <rwintgen@2C4.42angouleme.fr>
2024-11-26 14:46:56 +0100

    fixed mariadb init script

--- Inception/srcs/requirements/mariadb/mdb_init.sh
+++ Inception/srcs/requirements/mariadb/mdb_init.sh
@@ -5,10 +5,10 @@ service mysql start;
 
 # create and setup db
 mysql -u root << eof
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+"CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+"CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+"GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+"ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 eof
 
 # restart service
