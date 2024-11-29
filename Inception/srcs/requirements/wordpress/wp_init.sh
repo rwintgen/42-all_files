3dc582b955ffb0291cf564f448d503eafea2f114
Romain Wintgens <rwintgen@2E6.42angouleme.fr>
2024-11-29 18:16:00 +0100

    Wordpress + mariadb finally working

--- Inception/srcs/requirements/wordpress/wp_init.sh
+++ Inception/srcs/requirements/wordpress/wp_init.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # let mdb_init.sh finish
-sleep 10
+sleep 5
 
 # autofill wp startup fields
-wp config create	--allow-root \
-					--dbname=$SQL_DATABASE \
-					--dbuser=$SQL_USER \
-					--dbpass=$SQL_PASSWORD \
-					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+wp core download --path="/var/www/html" --allow-root
+wp config create --path="/var/www/html" --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=$DB_HOST --skip-check
+wp core install --path="/var/www/html" --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
+wp user create --path="/var/www/html" --allow-root $WP_USER $WP_EMAIL --user_pass=$WP_PASS --role='contributor'
 
 #launch php-fpm
-/usr/sbin/php-fpm7.4 -F
+exec /usr/sbin/php-fpm7.4 -F
